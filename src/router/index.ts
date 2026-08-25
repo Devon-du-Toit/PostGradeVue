f62269cdceb4bb00f897112641fd1cd8e4dacdc3
Devon <68278907+Devon-du-Toit@users.noreply.github.com>
2026-08-25 20:23:39 +0200

    Add gradebook route

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import DashboardView from '@/views/DashboardView.vue'
 import CoursesView from '@/views/CoursesView.vue'
 import CourseDetailView from '@/views/CourseDetailView.vue'
 import AssessmentDetailView from '@/views/AssessmentDetailView.vue'
+import GradebookView from '@/views/GradebookView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -43,6 +44,14 @@ const router = createRouter({
       path: '/courses/:id',
       name: 'course-detail',
       component: CourseDetailView,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/courses/:id/gradebook',
+      name: 'course-gradebook',
+      component: GradebookView,
       meta: {
         requiresAuth: true,
       },
