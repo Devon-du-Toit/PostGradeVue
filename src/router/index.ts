b969d047eec51575dbe8a7fb38a711fb4449a5a3
Devon <68278907+Devon-du-Toit@users.noreply.github.com>
2026-08-25 20:06:13 +0200

    Add courses route

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import DashboardView from '@/views/DashboardView.vue'
+import CoursesView from '@/views/CoursesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +25,14 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/courses',
+      name: 'courses',
+      component: CoursesView,
       meta: {
         requiresAuth: true,
       },
